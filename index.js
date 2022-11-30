26983eed87c4ee6c63965d6bf428f0c9490ab42d
Leandra Cartaxo <116728228+cartaxoleandra@users.noreply.github.com>
2022-11-30 12:23:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const registro= document.getElementById('registro')
+const name= document.getElementById('name')
+const login= document.getElementById('login')
+const email= document.getElementById('email')
+const password= document.getElementById('password')
+const passconfirmation= document.getElementById('passconfirmation')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+
+})
+function checkInputs(){
+    
+}
